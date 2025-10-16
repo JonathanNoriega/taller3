f0147890b69c23ef9ea88f6628c28caf4ed05032
Yeiner Ibarguen <y.ibarguenm@uniandes.edu.co>
2025-10-16 16:25:37 -0500

    hi

--- pruebasTaller3.sql
+++ pruebasTaller3.sql
@@ -27,9 +27,34 @@ DROP TABLE RESERVAS CASCADE CONSTRAINTS;
 
 
 Select * from reservas; --Cliente 1
-
+--Ningun problema al ejecutar
 Select * from reservas; --Cliente 2
 
-SET autocommit 1; --Cliente 1
+SET AUTOCOMMIT ON; --Cliente 1
+
+INSERT INTO RESERVAS (idReserva, idFuncion, idCliente, fecha, estado) VALUES (1, 1, 12345678, TO_DATE('2024-06-20', 'YYYY-MM-DD'), 'Reserva'); --Cliente 1
+
+SELECT * FROM RESERVAS; --Cliente 1
+
+
+
+BEGIN TRANSACTION;
+
+SET AUTOCOMMIT OFF;
+
+
+INSERT INTO Reservas (id_reserva, id_cliente, id_funcion, fecha_reserva)
+VALUES (id_reserva_param, id_cliente_param, id_funcion_param, SYSDATE);
+
+
+INSERT INTO SillasReservas (id_reserva, id_silla, fecha_creacion)
+VALUES (id_reserva_param, id_silla_1_param, SYSDATE);
+
+ 
+INSERT INTO SillasReservas (id_reserva, id_silla, fecha_creacion)
+VALUES (id_reserva_param, id_silla_2_param, SYSDATE);
+
+
+COMMIT;
 
  
